43ccbb3653b0770a11fd34bde085ddea952c47ab
Mac <vscherer86@gmail.com>
2023-08-02 23:17:34 -0300

    about fragment

--- app/src/main/java/com/example/consultancyappexercice/ui/about/AboutFragment.java
+++ app/src/main/java/com/example/consultancyappexercice/ui/about/AboutFragment.java
@@ -14,6 +14,9 @@ import android.view.ViewGroup;
 
 import com.example.consultancyappexercice.R;
 
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
 public class AboutFragment extends Fragment {
 
     private AboutViewModel mViewModel;
@@ -25,7 +28,26 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse " +
+                "a sollicitudin nunc. Integer sem dui, fringilla nec dolor id, imperdiet posuere justo. " +
+                "Ut pellentesque ligula urna, ut porttitor nisi convallis vel. Fusce faucibus neque semper, " +
+                "porttitor elit in, ornare dui. Sed rutrum consequat laoreet. Pellentesque sed accumsan mi. " +
+                "Quisque et vehicula tortor, ac porta urna. Praesent quis orci eu nisi pharetra fringilla in nec" +
+                " risus. Aliquam lacinia quis risus at tincidunt";
+
+        Element version = new Element();
+        version.setTitle("version 1.7.2");
+
+        return new AboutPage(getActivity())
+                .setImage(R.drawable.ssss_round)
+                .setDescription(description)
+                .addGroup("Contact")
+                .addEmail("sdmt@stmt.com", "email us!")
+                .addWebsite("https://www.twitch.tv/akemy_sama", "Our Website")
+                .addGroup("social media")
+                .addFacebook("vinicius.scherer.395", "Facebook") // URL ID
+                .addItem(version)
+                .create();
     }
 
     @Override
